b9be78208a505304e2365774f6ea6cb542655b92
konstantinos <konst.mastorakis@gmail.com>
2019-10-05 16:18:46 +0200

    Storing Building Semantics

--- CityJSON_Blender_parser.py
+++ CityJSON_Blender_parser.py
@@ -21,22 +21,17 @@ with open('/home/konstantinos/Thesis/cityjson_datasets/vienna.json') as json_fil
             y=vertex[1]*trans_param['scale'][1]+trans_param['translate'][1]
             z=vertex[2]*trans_param['scale'][2]+trans_param['translate'][2]
             vertices.append((x,y,z))
-    
     #Parsing the boundary data of every object
     for theid in data['CityObjects']:
-        #name = theid
-        
         # If a parent is found all children are parsed and visualized. 
         if 'children' in data['CityObjects'][theid]:
             #Storing parent's id
             parents_name = theid
             children_id = data['CityObjects'][theid]['children']
-            
             for child in children_id:
                 for geom in data['CityObjects'][child]['geometry']:
                     bound=list()
                     #Checking how nested the geometry is i.e what kind of 3D geometry it contains
-                   
                     if((geom['type']=='MultiSurface') or (geom['type'] == 'CompositeSurface')):
                         for face in geom['boundaries']:
                             for verts in face:
@@ -59,34 +54,27 @@ with open('/home/konstantinos/Thesis/cityjson_datasets/vienna.json') as json_fil
                 obj = bpy.data.objects.new(child, mesh_data)
                 scene = bpy.context.scene
                 scene.collection.objects.link(obj)
-                bpy.data.objects[child].select_set(True)
-                
+                #bpy.data.objects[child].select_set(True)
                 #Assigning attributes to chilren objects
                 if 'attributes' in data['CityObjects'][child]: 
                     for attribute in data['CityObjects'][child]['attributes']:
                         obj[attribute]=data['CityObjects'][child]['attributes'][attribute]        
-            
             #Creating empty meshes of the parents to join all the children    
             mesh_data = bpy.data.meshes.new("empty")
             obj = bpy.data.objects.new(parents_name, mesh_data)
             scene = bpy.context.scene
             scene.collection.objects.link(obj)
             #bpy.data.objects[parents_name].select_set(True)
-            
             #Assigning attributes to parent objects
             if 'attributes' in data['CityObjects'][theid]:
                 for attribute in data['CityObjects'][theid]['attributes']:
                         obj[attribute]=data['CityObjects'][theid]['attributes'][attribute]
-                
-                
-                          
             #Creating parent-child relationship
             objects = bpy.data.objects
             parent_obj = objects[parents_name]
             for child in children_id:
                 child_obj = objects[child]
                 child_obj.parent = parent_obj
-                      
         #Otherwise it searches for "orphan buildings" that have no parents and are not parents as well                                
         elif (('children' not in data['CityObjects'][theid]) and ('parents' not in data['CityObjects'][theid])):
             name = theid            
@@ -116,7 +104,6 @@ with open('/home/konstantinos/Thesis/cityjson_datasets/vienna.json') as json_fil
             scene = bpy.context.scene
             scene.collection.objects.link(obj)
             #bpy.data.objects[name].select_set(True)
-            
             #Assigning attributes to orphan objects
             if 'attributes' in data['CityObjects'][theid]:
                 for attribute in data['CityObjects'][theid]['attributes']:
--- Readme.txt
+++ Readme.txt
@@ -1,17 +1,40 @@
 # Blender-CityJSON-Plugin
 A Blender plugin to visualize 3D city models encoded in CityJSON format
 
-cityjson_parser.py --> A simple parser of CityJSON file just for importing the objects into Blender. No semantics or parent-child relationship is stored. 
+
+Blender Version 2.80 , Python Version 3.x
+__________________________________________
 
 
-cityjson_parser_parent-child.py --> The above parser augmented to also store the parent-child relation between buildings/building parts and all their attributes.
 
+Files included in the repository:
 
-shape_by_coords_spyder.py --> A simple test script to test the python code in a python console (except the commands that utilize the Blender API -bpy- library).
+1. CityJSON_Blender_parser.py --> Parser script
 
+2. attributes.png
+
+3. Readme.txt
 ______________________________________________________________________________________________
 
 
-How to use the parser(s): 1. Replace the directory of the CityJSON file in line 7 of the parsers scripts with your local directory. Enjoy! 
 
-2.To see the properties of each object simply select the object and click on the context.object tab on the bottom right. (Blender 2.80 interface). Then click the "Custom Properties" drop down menu. (See the screenshot "attributes.png")
+How to use the parser: 
+
+
+1. Open the script in the Blender API
+
+2.Replace the directory of the CityJSON file in line 7 of the parsers scripts with your local directory.
+
+3.Run the script
+
+4. Select a building / building part and make sure the object is a "cube_mesh_data.XXXX" and NOT "empty.XXXX". To check that click on the drop down symbol next to the ID of the object/building on the top right of the interface. Then click on the "View" tab and click "Frame Selected" (sometimes after "Frame Selected" is clicked you need to zoom in for the render to appear on your screen). 
+
+If the buildings behave weirdly as you rotate change the view "from Perspective" to "Orthographic" by clicking  the "numpad 5" key.
+
+If you want to visualize a certain area, click "Shift + B" and draw a rectangle with your mouse to zoom in into that specific area of the dataset.
+
+
+5. To select all the children of a building right click on the building's (parent's) ID and click "Select Hierarchy"
+
+6.To see the properties of each object simply select the object and click on the context.object tab on the bottom right (Blender 2.80 interface). Then click the "Custom Properties" drop down menu. (See the screenshot "attributes.png" for more info.)
+
