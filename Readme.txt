2f8b4fec76c2fba3df48fcf16e2cb0349b048bf4
konstantinos <konst.mastorakis@gmail.com>
2019-10-05 15:41:29 +0200

    Storing building semantics

--- Readme.txt
+++ Readme.txt
@@ -4,12 +4,14 @@ A Blender plugin to visualize 3D city models encoded in CityJSON format
 cityjson_parser.py --> A simple parser of CityJSON file just for importing the objects into Blender. No semantics or parent-child relationship is stored. 
 
 
-cityjson_parser_parent-child.py --> The above parser augmented to also store the parent-child relation between buildings/building parts.
+cityjson_parser_parent-child.py --> The above parser augmented to also store the parent-child relation between buildings/building parts and all their attributes.
 
 
-shape_by_coords_spyder.py --> A simple test script to test the python code (except the commands that utilize the Blender API -bpy- library).
+shape_by_coords_spyder.py --> A simple test script to test the python code in a python console (except the commands that utilize the Blender API -bpy- library).
 
 ______________________________________________________________________________________________
 
 
-How to use the parser(s): Replace the directory of the CityJSON file in line 7 of the parsers scripts with your local directory. Enjoy! 
+How to use the parser(s): 1. Replace the directory of the CityJSON file in line 7 of the parsers scripts with your local directory. Enjoy! 
+
+2.To see the properties of each object simply select the object and click on the context.object tab on the bottom right. (Blender 2.80 interface). Then click the "Custom Properties" drop down menu. (See the screenshot "attributes.png")
--- cityjson_parser_parent-child.py
+++ cityjson_parser_parent-child.py
@@ -4,6 +4,7 @@ import json
 bpy.ops.object.select_all(action="SELECT")
 bpy.ops.object.delete(use_global=False)
 
+#Open CityJSON file
 with open('/home/konstantinos/Thesis/cityjson_datasets/vienna.json') as json_file: 
     data = json.load(json_file)
     vertices=list()
@@ -21,108 +22,92 @@ with open('/home/konstantinos/Thesis/cityjson_datasets/vienna.json') as json_fil
             z=vertex[2]*trans_param['scale'][2]+trans_param['translate'][2]
             vertices.append((x,y,z))
     
-   
-    
-    
-        #Parsing the boundary data of every object
+    #Parsing the boundary data of every object
     for theid in data['CityObjects']:
         #name = theid
         
-        # If it finds a parent parses all its children and visualizes them. 
+        # If a parent is found all children are parsed and visualized. 
         if 'children' in data['CityObjects'][theid]:
             #Storing parent's id
             parents_name = theid
             children_id = data['CityObjects'][theid]['children']
             
             for child in children_id:
-                
                 for geom in data['CityObjects'][child]['geometry']:
                     bound=list()
                     #Checking how nested the geometry is i.e what kind of 3D geometry it contains
                    
                     if((geom['type']=='MultiSurface') or (geom['type'] == 'CompositeSurface')):
-                        
                         for face in geom['boundaries']:
                             for verts in face:
                                 bound.append(tuple(verts))
-                                
                     elif (geom['type']=='Solid'):
-                        
                         for shell in geom['boundaries']:
                             for face in shell:
                                 for verts in face:
-                                    bound.append(tuple(verts))
-                                    
+                                    bound.append(tuple(verts))                                    
                     elif (geom['type']=='MultiSolid'):
-                        
                         for solid in geom['boundaries']:
                             for shell in solid:
                                 for face in shell:
                                     for verts in face:
                                         bound.append(tuple(verts))
                 #Visualization part
-                mesh_data = bpy.data.meshes.new("cube_mesh_data")
+                mesh_data = bpy.data.meshes.new("mesh")
                 mesh_data.from_pydata(vertices, [], bound)
                 mesh_data.update()
                 obj = bpy.data.objects.new(child, mesh_data)
                 scene = bpy.context.scene
                 scene.collection.objects.link(obj)
                 bpy.data.objects[child].select_set(True)
-            
+                
+                #Assigning attributes to chilren objects
+                if 'attributes' in data['CityObjects'][child]: 
+                    for attribute in data['CityObjects'][child]['attributes']:
+                        obj[attribute]=data['CityObjects'][child]['attributes'][attribute]        
             
             #Creating empty meshes of the parents to join all the children    
-            mesh_data = bpy.data.meshes.new("cube_mesh_data")
+            mesh_data = bpy.data.meshes.new("empty")
             obj = bpy.data.objects.new(parents_name, mesh_data)
             scene = bpy.context.scene
             scene.collection.objects.link(obj)
             #bpy.data.objects[parents_name].select_set(True)
             
-            
-            
-            
-            #Setting parent child relationship NOT geometrically
+            #Assigning attributes to parent objects
+            if 'attributes' in data['CityObjects'][theid]:
+                for attribute in data['CityObjects'][theid]['attributes']:
+                        obj[attribute]=data['CityObjects'][theid]['attributes'][attribute]
+                
+                
+                          
+            #Creating parent-child relationship
             objects = bpy.data.objects
             parent_obj = objects[parents_name]
             for child in children_id:
                 child_obj = objects[child]
                 child_obj.parent = parent_obj
-            
-            
-            
-            
-            
- 
-                   
-        
+                      
         #Otherwise it searches for "orphan buildings" that have no parents and are not parents as well                                
         elif (('children' not in data['CityObjects'][theid]) and ('parents' not in data['CityObjects'][theid])):
-            name = theid
-            
+            name = theid            
             for geom in data['CityObjects'][theid]['geometry']:
                 bound=list()
                 #Checking how nested the geometry is i.e what kind of 3D geometry it contains
-               
                 if((geom['type']=='MultiSurface') or (geom['type'] == 'CompositeSurface')):
-                    
                     for face in geom['boundaries']:
                         for verts in face:
                             bound.append(tuple(verts))
-                            
                 elif (geom['type']=='Solid'):
-                    #name = theid
                     for shell in geom['boundaries']:
                         for face in shell:
                             for verts in face:
                                 bound.append(tuple(verts))
-                                
                 elif (geom['type']=='MultiSolid'):
-                    #name = theid
                     for solid in geom['boundaries']:
                         for shell in solid:
                             for face in shell:
                                 for verts in face:
                                     bound.append(tuple(verts))
-                
             #Visualization part
             mesh_data = bpy.data.meshes.new("cube_mesh_data")
             mesh_data.from_pydata(vertices, [], bound)
@@ -131,11 +116,13 @@ with open('/home/konstantinos/Thesis/cityjson_datasets/vienna.json') as json_fil
             scene = bpy.context.scene
             scene.collection.objects.link(obj)
             #bpy.data.objects[name].select_set(True)
+            
+            #Assigning attributes to orphan objects
+            if 'attributes' in data['CityObjects'][theid]:
+                for attribute in data['CityObjects'][theid]['attributes']:
+                        obj[attribute]=data['CityObjects'][theid]['attributes'][attribute]
         
         
-#objects = bpy.data.objects
-#a = objects['UUID_LOD2_011390-b5d493d1-4222-4f08-8fa2']
-#b = objects['UUID_LOD2_011390-b5d493d1-4222-4f08-8fa2_1']
-#b.parent = a
     #bpy.ops.object.light_add(type='SUN', location=(91231.52200000001, 435866.023, 55.085))
     #bpy.ops.object.camera_add(enter_editmode=False, align='VIEW', location=(91531.52200000001, 435866.023, 15.085), rotation=(0.814687, 0.008346, 1.16357))
+
