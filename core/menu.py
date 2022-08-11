3d857886c528facfde0b709b602abb602bab4b66
kvlahrobal <tim.balschmiter@rostock.de>
2022-08-11 10:54:05 +0200

    Update menu.py
    Menü erstellt

--- core/menu.py
+++ core/menu.py
@@ -1,4 +1,6 @@
 import bpy
+import pprint
+import idprop
 
 class VIEW3D_MT_edit_mesh_context_submenu(bpy.types.Menu):
     bl_label = 'SurfaceTypes'
@@ -8,9 +10,10 @@ class VIEW3D_MT_edit_mesh_context_submenu(bpy.types.Menu):
         layout = self.layout
         layout.label(text="Building")
         # bei den folgenden Operatoren muss ein Weg gefunden werden 
-        layout.operator(SetSurfaceOperator.bl_idname, text="GroundSurface")
-        layout.operator(SetSurfaceOperator.bl_idname, text="WallSurface")
-        layout.operator(SetSurfaceOperator.bl_idname, text="RoofSurface")
+        #layout.operator(SetSurfaceOperator.bl_idname, text="GroundS").surfaceType = "GroundSurface"
+        layout.operator(SetSurfaceOperator.bl_idname, text="GroundSurface").surfaceType = 'GroundSurface'
+        layout.operator(SetSurfaceOperator.bl_idname, text="WallSurface").surfaceType = 'WallSurface'
+        layout.operator(SetSurfaceOperator.bl_idname, text="RoofSurface").surfaceType = 'RoofSurface'
 
 class VIEW3D_MT_edit_mesh_context_menu(bpy.types.Menu):
     bl_label = ''
@@ -21,11 +24,22 @@ class VIEW3D_MT_edit_mesh_context_menu(bpy.types.Menu):
 
 class SetSurfaceOperator(bpy.types.Operator):
     bl_idname = "wm.set_surface"
-    bl_label = "Minimal Operator"
-    
+    bl_label = "SetSurfaceOperator"
+    surfaceType: bpy.props.StringProperty(
+        name = 'surfaceType',
+        default = ''
+    )
     def execute(self, context):
-        print("Hier soll die Unterscheidung zwischen den Types getroffen werden und entsprechend die Materialfunction aufgerufen werden")
-        print(self.text)
+        print(self.surfaceType)
+        obj = bpy.context.object
+        if obj.type == 'MESH':
+            mesh = obj.data # Assumed that obj.type == 'MESH'
+            obj.update_from_editmode() # Loads edit-mode data into object data
+            selected_polygons = [p for p in mesh.polygons if p.select]
+            for face in selected_polygons:
+                print ("Huhu")
+                #print (face.id)
+                #für jede Fläche sollen nun die entsprechenden Materialien und CustomProperties angelegt werden
         return {'FINISHED'}
 
 classes = (
@@ -39,7 +53,7 @@ def menu_func(self, context):
         layout = self.layout
         layout.separator()
         layout.label(text="CityJSON Options")
-        layout.menu(VIEW3D_MT_edit_mesh_context_submenu.bl_idname, text="set SurfaceType3")
+        layout.menu(VIEW3D_MT_edit_mesh_context_submenu.bl_idname, text="set SurfaceType")
 
 def register():
     print("Register")
